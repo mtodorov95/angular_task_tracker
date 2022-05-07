f62b9c78c305aa8e010dfca6d1214ac1c2506ac3
mtodorov <mtodorov95@gmail.com>
2022-05-07 10:46:39 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // HTML Tag
   selector: 'app-root',
+  // HTML file
   templateUrl: './app.component.html',
+  // CSS file
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'crash-course';
+  
 }
