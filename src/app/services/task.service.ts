df764972639bd190137a57a81b030e3b72e19402
mtodorov <mtodorov95@gmail.com>
2022-05-07 11:07:47 +0300

    * json-server backend

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Task } from '../Task';
 import { TASKS } from '../mock-task';
 
@@ -8,10 +9,11 @@ import { TASKS } from '../mock-task';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl:string = "http://localhost:5000/tasks";
+
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
